491c2c13975ceb51ea9a9388d5ab909f61a457d6
brodyreid <brody.reid@pivotfurniture.com>
2022-06-23 09:58:37 -0400

    types

---
+++ dist/types/entities/circle.d.ts
@@ -0,0 +1,14 @@
+import DxfArrayScanner, { IGroup } from '../DxfArrayScanner.js';
+import IGeometry, { IEntity, IPoint } from './geomtry.js';
+export interface ICircleEntity extends IEntity {
+    center: IPoint;
+    radius: number;
+    startAngle: number;
+    endAngle: number;
+    angleLength: number;
+}
+export default class Circle implements IGeometry {
+    ForEntityName: "CIRCLE";
+    parseEntity(scanner: DxfArrayScanner, curr: IGroup): ICircleEntity;
+}
+//# sourceMappingURL=circle.d.ts.map
---
+++ dist/types/entities/solid.d.ts
@@ -0,0 +1,11 @@
+import DxfArrayScanner, { IGroup } from '../DxfArrayScanner.js';
+import IGeometry, { IEntity, IPoint } from './geomtry.js';
+export interface ISolidEntity extends IEntity {
+    points: IPoint[];
+    extrusionDirection: IPoint;
+}
+export default class Solid implements IGeometry {
+    ForEntityName: "SOLID";
+    parseEntity(scanner: DxfArrayScanner, curr: IGroup): ISolidEntity;
+}
+//# sourceMappingURL=solid.d.ts.map
